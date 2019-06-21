8422342593d0b78e6812ef8948cc8f32844c4bb8
Robert Bolton <therobshock@yahoo.com>
2019-06-20 23:20:13 -0700

    Updates

---
+++ precache-manifest.745f912cc7a6c4386ce05a357dca3c68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad634f866e04a2b6e7cc9cb5bf5d4844",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9d34c6f22b49e33898dc",
+    "url": "/clicky-game/static/css/main.bdb02a85.chunk.css"
+  },
+  {
+    "revision": "008b3e8efd6e7295fcc6",
+    "url": "/clicky-game/static/js/2.675a6941.chunk.js"
+  },
+  {
+    "revision": "9d34c6f22b49e33898dc",
+    "url": "/clicky-game/static/js/main.45ad464d.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
